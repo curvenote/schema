a7efb67bb14a74dc3f0579ed910184031dea0e01
stevejpurves <steve@curvenote.com>
2022-03-21 14:29:05 +0000

    🧂 finalising long tables, figure attributes and added tests

--- src/serialize/types.ts
+++ src/serialize/types.ts
@@ -52,6 +52,7 @@ export interface TexSerializerState extends SharedSerializerState {
   nextCaptionNumbered?: boolean;
   isInTable?: boolean;
   containsTable?: boolean;
+  longFigure?: boolean;
 }
 
 export type MdFormatSerialize<S extends Schema<any, any> = any> = (
